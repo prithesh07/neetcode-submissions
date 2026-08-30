5302940f5c9df6223f4e81e8dae248898c8fbb79
Prithesh <prithesh07@gmail.com>
2026-08-30 00:00:17 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.go
@@ -0,0 +1,18 @@
+func maxProfit(prices []int) int {
+	n := len(prices)
+	left, right := 0, 0
+	max_profit := 0
+
+	for right < n {
+		profit := prices[right] - prices[left]
+
+		if profit >= 0 {
+			max_profit = max(max_profit, profit)
+		} else {
+			left = right
+		}
+		right++
+	}
+
+	return max_profit
+}
